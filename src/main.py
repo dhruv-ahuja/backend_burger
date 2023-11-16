d22f22d1c5e48b4a47b7745c2e285c98ec37fca3
Dhruv Ahuja <83733638+dhruv-ahuja@users.noreply.github.com>
2023-11-16 08:19:21 +0530

    feat: add health check endpoint

--- src/main.py
+++ src/main.py
@@ -4,3 +4,10 @@ from .config import setup_services
 
 
 app = FastAPI(lifespan=setup_services)
+
+
+@app.get("/")
+async def get():
+    """Returns a simple success message indicating that the server is up and running."""
+
+    return {"status": "ok"}
