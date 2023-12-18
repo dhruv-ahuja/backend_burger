ffa6221b47dcc6e1df180a1b5d7095a7d88bb551
Dhruv Ahuja <83733638+dhruv-ahuja@users.noreply.github.com>
2023-12-18 22:23:54 +0530

    chore: integrate loguru with caplog for pytest

---
+++ src/tests/conftest.py
@@ -0,0 +1,26 @@
+from loguru import logger
+import pytest
+from _pytest.logging import LogCaptureFixture
+
+
+# integrates loguru with caplog
+@pytest.fixture
+def caplog(caplog: LogCaptureFixture):
+    handler_id = logger.add(
+        caplog.handler,
+        format="{message}",
+        level=0,
+        filter=lambda record: record["level"].no >= caplog.handler.level,
+        enqueue=False,  # Set to 'True' if your test is spawning child processes.
+    )
+    yield caplog
+    logger.remove(handler_id)
+
+
+# propagates logger statements to pytest terminal output
+@pytest.fixture
+def reportlog(pytestconfig):
+    logging_plugin = pytestconfig.pluginmanager.getplugin("logging-plugin")
+    handler_id = logger.add(logging_plugin.report_handler, format="{message}")
+    yield
+    logger.remove(handler_id)
