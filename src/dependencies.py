c15485ca5011d81f59d51483f75999d81ddb08f8
Dhruv Ahuja <83733638+dhruv-ahuja@users.noreply.github.com>
2024-01-07 21:09:34 +0530

    fix: raise error immediately if invalid access token

--- src/dependencies.py
+++ src/dependencies.py
@@ -16,30 +16,29 @@ from src.services import users as users_service, auth as auth_service
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 
-async def check_access_token(access_token: str = Depends(oauth2_scheme)) -> dict[str, Any] | None:
+async def check_access_token(access_token: str = Depends(oauth2_scheme)) -> dict[str, Any]:
     """Checks whether the access token was signed by this server and whether its still valid.
     Returns `None` if the token is invalid, else the token data dictionary."""
+
+    forbidden_error = HTTPException(status.HTTP_403_FORBIDDEN)
 
     try:
         token_data = auth_utils.parse_access_token(access_token)
     except JWTError:
-        return None
+        raise forbidden_error
 
     blacklisted_token = await auth_service.get_blacklisted_token(access_token)
     if blacklisted_token is not None:
-        return None
+        raise forbidden_error
 
     return token_data
 
 
-async def get_current_user(token_data: dict[str, Any] | None = Depends(check_access_token)) -> User:
+async def get_current_user(token_data: dict[str, Any] = Depends(check_access_token)) -> User:
     """Checks whether the `user_id` inside the token is valid and whether the user exists or not, returning the user
     instance. Raises a 403 error if any of the checks fails."""
 
     forbidden_error = HTTPException(status.HTTP_403_FORBIDDEN)
-
-    if token_data is None:
-        raise forbidden_error
 
     try:
         user_id = PydanticObjectId(token_data["sub"])
