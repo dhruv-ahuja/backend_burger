f664c948be65e141b5d90c0a0afa10b33059f3b7
Dhruv Ahuja <83733638+dhruv-ahuja@users.noreply.github.com>
2024-06-22 00:55:34 +0530

    feat: predict future item prices

--- src/scripts/price_prediction.py
+++ src/scripts/price_prediction.py
@@ -60,3 +60,44 @@ async def get_price_history_data(category_internal_name: str, item_id: int):
         return []
 
     return price_history_data
+
+
+def predict_future_item_prices(data: list[PriceHistoryEntity], days: int = 4):
+    df = pd.DataFrame([entity.model_dump() for entity in data])
+
+    # Filter and keep only the last 30 days' data
+    df = df.tail(30)
+    df["index"] = range(1, len(df) + 1)
+
+    # Set the independent variables (X) and add a constant term
+    # NOTE: integrating count (extra variable) would require a more complex model; linear regression takes in 1 value
+    X = df[["days_ago"]]
+    X = sm.add_constant(X)
+
+    # Set the dependent variable (Y)
+    Y = df["value"]
+
+    # Set up PolynomialFeatures and fit_transform X
+    poly = PolynomialFeatures(degree=2, include_bias=False)
+    X_poly = poly.fit_transform(X.drop(columns=["const"]))  # type: ignore
+
+    # Fit polynomial regression model
+    model_poly = LinearRegression()
+    model_poly.fit(X_poly, Y)
+
+    # Prepare future data for predictions
+    future_days_ago = [df["days_ago"].min() + i for i in range(1, 5)]  # Predicting for next 4 days
+    future_count = df["count"].mean()
+
+    future_data = pd.DataFrame({"days_ago": future_days_ago, "count": [future_count] * len(future_days_ago)})
+    future_data = pd.DataFrame({"days_ago": future_days_ago})
+
+    future_data_with_const = sm.add_constant(future_data, has_constant="add")  # Add constant term for predictions
+
+    # Transform future data for polynomial regression
+    future_X_poly = poly.transform(future_data_with_const.drop(columns=["const"]))  # type: ignore
+
+    # Predict future values using both models
+    predictions = model_poly.predict(future_X_poly)
+
+    return predictions
