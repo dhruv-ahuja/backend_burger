59fdd436b66e00ce241eaf9c30339ff10359549c
Dhruv Ahuja <83733638+dhruv-ahuja@users.noreply.github.com>
2024-07-10 21:59:54 +0530

    feat: implement filter-sort and pagination

--- src/routers/poe.py
+++ src/routers/poe.py
@@ -1,17 +1,18 @@
 from fastapi import APIRouter, Depends, Query
 
 from src import dependencies as deps
-from src.schemas.web_responses import users as resp
+from src.schemas.requests import FilterSortInput, PaginationInput
+from src.schemas.web_responses import poe as resp
 from src.schemas.responses import AppResponse, BaseResponse
 from src.services import poe as service
+from src.utils.routers import create_pagination_response
 
 
 dependencies = [Depends(deps.check_access_token)]
 router = APIRouter(prefix="/poe", tags=["Path of Exile"], dependencies=dependencies)
 
 
-# TODO: add responses
-@router.get("/categories", responses=resp.GET_USERS_RESPONSES)
+@router.get("/categories", responses=resp.GET_CATEGORIES_RESPONSES)
 async def get_all_categories():
     """Gets a list of all item categories from the database, mapped by their group names."""
 
@@ -21,9 +22,15 @@ async def get_all_categories():
     return AppResponse(BaseResponse(data=item_category_mapping))
 
 
-@router.get("/items", responses=resp.GET_USERS_RESPONSES)
-async def get_items_by_group(category_group: str = Query(..., min_length=3, max_length=50)):
+@router.get("/items", responses=resp.GET_ITEMS_RESPONSES)
+async def get_items_by_group(
+    category_group: str | None = Query(None, min_length=3, max_length=50),
+    pagination: PaginationInput = Depends(),  # using Depends allows us to encapsulate Query params within Pydantic models
+    filter_sort_input: FilterSortInput | None = None,
+):
     """Gets a list of all items belonging to the given category group."""
 
-    items = await service.get_items_by_group(category_group)
-    return AppResponse(BaseResponse(data=items))
+    items, total_items = await service.get_items(category_group, pagination, filter_sort_input)
+
+    response = create_pagination_response(items, total_items, pagination, "items")
+    return AppResponse(response)
--- src/services/poe.py
+++ src/services/poe.py
@@ -6,6 +6,8 @@ from starlette import status
 
 from src.models.poe import Item, ItemCategory
 from src.schemas.poe import ItemBase, ItemCategoryResponse
+from src.schemas.requests import FilterSortInput, PaginationInput
+from src.utils.services import QueryChainer
 
 
 async def get_item_categories() -> list[ItemCategoryResponse]:
@@ -31,31 +33,58 @@ def group_item_categories(item_categories: list[ItemCategoryResponse]) -> dict[s
     return item_category_mapping
 
 
-async def get_items_by_group(category_group: str) -> list[ItemBase]:
+async def get_items(
+    category_group: str | None, pagination: PaginationInput, filter_sort_input: FilterSortInput | None
+) -> tuple[list[ItemBase], int]:
     """
-    Gets items by given category group. Raises a 400 error if category group is invalid.
+    Gets items by given category group, and the total items' count in the database. Raises a 400 error if category
+    group is invalid.
     """
 
-    try:
-        item_category = await ItemCategory.find_one(ItemCategory.group == category_group)
-    except Exception as exc:
-        logger.error(f"error getting item category by group '{category_group}': {exc} ")
-        raise
+    item_category = None
+    if category_group is not None:
+        try:
+            item_category = await ItemCategory.find_one(ItemCategory.group == category_group)
+        except Exception as exc:
+            logger.error(f"error getting item category by group '{category_group}': {exc} ")
+            raise
+
+        if item_category is None:
+            raise HTTPException(status.HTTP_400_BAD_REQUEST, "Invalid category group.")
 
     if item_category is None:
-        raise HTTPException(status.HTTP_400_BAD_REQUEST, "Invalid category group.")
+        query = Item.find()
+    else:
+        query = Item.find(Item.category.group == category_group)  # type: ignore
+
+    chainer = QueryChainer(query, Item)
+
+    if filter_sort_input is None:
+        items_count = await query.find(fetch_links=True).count()
+        items = await chainer.paginate(pagination).query.find(fetch_links=True).project(ItemBase).to_list()
+
+        return items, items_count
+
+    base_query_chain = chainer.filter(filter_sort_input.filter_).sort(filter_sort_input.sort)
+
+    # * clone the query for use with total record counts and pagination calculations
+    count_query = (
+        base_query_chain.filter(filter_sort_input.filter_)
+        .sort(filter_sort_input.sort)
+        .clone()
+        .query.find(fetch_links=True)
+        .count()
+    )
+
+    paginated_query = base_query_chain.paginate(pagination).query.find(fetch_links=True).project(ItemBase).to_list()
 
     try:
-        items = (
-            await Item.find(
-                Item.category.group == category_group,  # type: ignore
-                fetch_links=True,
-            )
-            .project(ItemBase)
-            .to_list()
-        )
+        items = await paginated_query
+        items_count = await count_query
     except Exception as exc:
-        logger.error(f"error getting item by category group '{category_group}': {exc}")
+        logger.error(
+            f"error getting items from database category_group:'{category_group}'; filter_sort: {filter_sort_input}: {exc}"
+        )
         raise
 
-    return items
+    return items, items_count
